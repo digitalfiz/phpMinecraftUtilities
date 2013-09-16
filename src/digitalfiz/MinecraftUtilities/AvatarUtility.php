9ac39e9ea40da0868f9ca3effc267b43faf53657
Marc Seiler <mseiler@gmail.com>
2013-09-16 18:43:34 -0400

    more simple not sim...

--- src/digitalfiz/MinecraftUtilities/AvatarUtility.php
+++ src/digitalfiz/MinecraftUtilities/AvatarUtility.php
@@ -5,7 +5,7 @@
  * 
  * A lot of the code for `getAvatarFace` and `getFullAvatar` was adapted from work by Jamie Bicknell and his 
  * Minecraft-Avatar github repo at: https://github.com/jamiebicknell/Minecraft-Avatar Check it out if you want a much
- * similair implimentation.
+ * more simple implimentation.
  * 
  * @package  minecraft
  */
